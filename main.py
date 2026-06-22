b654768a84c26f16bcac99a99b2f19a8c7125052
Madhavi <madhviii0911@gmail.com>
2026-06-22 22:45:19 +0530

    added main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+import json
 app = FastAPI()
 @app.get("/")
-def home():
-    return {"message" : "Welcome to fastAPI Series!"}
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+    return data
+        
+        
+def hello():
+    return {"message" : "Patient Management System API"}
 @app.get("/contact")
-def contact():
-    return {"message" : "You can connect us any time."}
+def about():
+    return {"message" : "A fully functional API to manage your patient record."}
+@app.get('/view')
+def view():
+    data = load_data()
+    return data
